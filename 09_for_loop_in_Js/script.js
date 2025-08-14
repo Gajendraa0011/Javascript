7cd672f93f3bdf82c7a867c3093397e8b29a412e
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-08-14 21:11:54 +0530

    Update script.js

--- 09_for_loop_in_Js/script.js
+++ 09_for_loop_in_Js/script.js
@@ -1,6 +1,8 @@
-// for (let i = 0; i <= 5; i++) {
-//   console.log(i + 1);
-// }
+/*
+for (let i = 0; i <= 5; i++) {
+console.log(i + 1);
+}
+*/
 
 // programm to add first n natural number
 // let sum = 0;
@@ -13,16 +15,15 @@
 
 //for in loop agar key value pair ho to kam le skte hai
 let obj = {
-    shivika: 92,
-    radhika: 98,
-    kanika: 93,
-    rashika: 95,
-    shiradvika: 94,
-}
+  shivika: 92,
+  radhika: 98,
+  kanika: 93,
+  rashika: 95,
+  shiradvika: 94,
+};
 
+for (let a in obj) {
+  // console.log(a);
 
-for (let a in obj){
-    // console.log(a);
-
-    console.log("the marks of " + a + " are " + obj[a]);
+  console.log("the marks of " + a + " are " + obj[a]);
 }
