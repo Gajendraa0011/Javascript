93f0ef3215b838a396827f6692e0121bbde86829
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-08-18 07:01:26 +0530

    updated

---
+++ 11_Functions_in_Js/script.js
@@ -0,0 +1,16 @@
+function avg(x, y) {
+  return (x + y) / 2;
+}
+
+let a = 4;
+let b = 5;
+let c = 6;
+console.log("the avg of a and b is ", avg(a, b));
+
+// https://replit.com/@codewithharry/11functions#index.js
+
+const sum = (p, q) => {
+  return p + q;
+};
+
+console.log(sum(7, 2));
