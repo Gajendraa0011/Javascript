00960f47bbbed8ed6a62b5b26457bd3cc42e7ee9
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-08-18 07:35:22 +0530

    ok

---
+++ 13_Strings_in_Js/script.js
@@ -0,0 +1,16 @@
+let full_name = "radhika";
+console.log(full_name.length);
+
+console.log(full_name[0]);
+console.log(full_name[1]);
+console.log(full_name[2]);
+console.log(full_name[3]);
+console.log(full_name[4]);
+console.log(full_name[5]);
+console.log(full_name[6]);
+
+let boy1 = "Krishna";
+let boy2 = "Radhika";
+
+let sentence = `${boy1} is friend of ${boy2}`;
+console.log(sentence);
