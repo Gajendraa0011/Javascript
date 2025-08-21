a300edb997bb17120d3311fcd722e509996f86ed
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-08-21 06:30:51 +0530

    done

---
+++ 14_Strings_method_in_Js/script.js
@@ -0,0 +1,14 @@
+let name = "Harry";
+// console.log(name.length);
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,4))
+// console.log(name.replace("Har","Per"))
+
+let friend = "krishna";
+console.log(name.concat(" is a friend of ", friend, " ok"));
+
+let dost = "         Radhika";
+console.log(dost)
+console.log(dost.trim())
