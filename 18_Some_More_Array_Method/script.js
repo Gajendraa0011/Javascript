1fa2bb6ec8cf927dc35d0d92c3a84ed3ab887885
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-08-28 08:46:59 +0530

    updated

--- 18_Some_More_Array_Method/script.js
+++ 18_Some_More_Array_Method/script.js
@@ -1,14 +1,37 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(num.length);
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(num.length);
 
-delete num[0];
-console.log(num);
+// delete num[0];
+// console.log(num);
 
-console.log(num.length);
-
-let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19];
-let num_even_more = [111, 112, 113, 114, 115, 116, 117, 118, 119];
-let newArray = num.concat(num_more, num_even_more);
-console.log(newArray);
+// console.log(num.length);
+//concat method
+// let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+// let num_even_more = [111, 112, 113, 114, 115, 116, 117, 118, 119];
+// let newArray = num.concat(num_more, num_even_more);
+// console.log(newArray);
 
 //sort method
+
+// let num = [133, 112, 243, 264, 357, 376, 479, 449, 594];
+// num.sort();
+// console.log(num);
+
+// let compare = (a, b) => {
+//   return a - b;
+// };
+
+// let n = [1,2,3,4,5,6,7,8,9];
+// num.sort(compare);
+// console.log(num);
+
+//  splice
+
+let n = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let delVal = n.splice(2, 3, 10, 20, 30, 40);
+// console.log(n);
+// console.log(delVal);
+
+// let newArray = n.slice(3);
+let newArray = n.slice(3,5);
+console.log(newArray);
