9ef740edc23eb8ae57088168ef5e491b976779f1
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-09-03 08:39:20 +0530

    updated

--- 20_map_filter_reduce/script.js
+++ 20_map_filter_reduce/script.js
@@ -1,4 +1,21 @@
 let arr = [22, 44, 66];
-arr.map((value,index,array) => {
-  console.log(value,index,array);
+arr.map((value, index, array) => {
+  console.log(value, index, array);
 });
+
+// Array filter method
+let arr2 = [33, 45, 76, 1, 2, 3, 4, 5];
+let a2 = arr2.filter((value) => {
+  return value < 10;
+});
+
+console.log(a2);
+
+//Array reudce method
+
+let arr3 = [1, 2, 3, 4, 5, 6];
+let a3 = arr3.reduce((x, y) => {
+  return x + y;
+});
+
+console.log(a3);
