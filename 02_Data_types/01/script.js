b275f727e99d1409972f248ffd63ffc83c283c37
Gajendraa0011 <109782488+Gajendraa0011@users.noreply.github.com>
2025-12-09 08:49:09 +0530

    ok

---
+++ 02_Data_types/01/script.js
@@ -0,0 +1,6 @@
+// primitives types -> aisi sari values jinko copy karne par apko ek real copy mil jaye 
+// string, number, boolean, null, undefined, symbol, bigint
+
+// reference -> inko copy karne real copy nahi milegi but apko reference milega parent ka
+//arrys object functions
+// []    {}       ()
